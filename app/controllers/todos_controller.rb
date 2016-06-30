0eac6f64330cffea449eeb29e4c84d473dc39f32
AkyunaAkish <akyunaakish@gmail.com>
2016-06-30 15:31:53 -0600

    initial commit

--- app/controllers/todos_controller.rb
+++ app/controllers/todos_controller.rb
@@ -3,17 +3,18 @@ class TodosController < ApplicationController
   def index
     confirm = JsonWebToken.verify(params[:jwt], key: ENV['SECRET'])
     render json: Todo.where(user_id: confirm[:ok][:id])
+    rescue
+      render json: {'error': 'invalid JWT'}
   end
 
   def create
-    if params[:user_id] && params[:jwt] && params[:text] # check token!!!! PLEASE
-      todo = Todo.new(user_id: params[:user_id], text: params[:text])
-      todo.completed = false
-      todo.save!
-      render json: todo
-    else
-      render status: 400, json: {error: "Todo could not be created"}
-    end
+    confirm = JsonWebToken.verify(params[:jwt], key: ENV['SECRET'])
+    todo = Todo.new(user_id: confirm[:ok][:id], text: params[:text])
+    todo.completed = false
+    todo.save!
+    render json: todo
+  rescue
+    render json: {'error': 'invalid JWT'}
   end
 
 end
